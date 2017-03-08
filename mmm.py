89b2c7c83f5ab0016034d81ea687e22b902956ba
William Pereira <william@williampereira.me>
2017-03-08 10:53:52 -0300

    Compatibility with Python 3

--- mmm.py
+++ mmm.py
@@ -12,23 +12,23 @@ logData = []
 for line in logLines:
     logData.append(line.rstrip().split('|',5))
 
-# Number of clicks	
+# Number of clicks
 nc = 0
 for logEntry in logData:
 	if logEntry[4] == " Left Click Down":
 		nc = nc + 1
-	
-print "Number of Clicks: " + str(nc)
-	
-# Task time 
+
+print ("Number of Clicks: " + str(nc))
+
+# Task time
 tt = 0
 for logEntry in logData:
 	tt = tt + int(logEntry[3])
 
-print "Task time: " + str(tt) + " ms"
- 
-# Mean wait time between clicks			
-print "Mean Wait Time Between Clicks: " + str(tt/nc) + " ms"
+print ("Task time: " + str(tt) + " ms")
+
+# Mean wait time between clicks
+print ("Mean Wait Time Between Clicks: " + str(tt/nc) + " ms")
 
 # Mouse distance covered
 md = 0
@@ -38,7 +38,7 @@ for logEntry in logData:
 		p2 = (int(logEntry[1]),int(logEntry[2]))
 		md = md + math.hypot(p2[0] - p1[0], p2[1] - p1[1])
 		p1 = p2
-	
-print "Mouse distance covered: " + str(int(md))
 
-	
+print ("Mouse distance covered: " + str(int(md)))
+
+
